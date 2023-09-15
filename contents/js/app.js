a30dfc2f3bd9e9f83f6b70057f4994b5af140c83
richardpromise <richardpromise013@gmail.com>
2023-09-15 12:32:05 +0100

    update

--- contents/js/app.js
+++ contents/js/app.js
@@ -370,8 +370,8 @@ function reset() {
       }
       while (newRepo.firstChild) {
         newRepo.removeChild(newRepo.firstChild);
-        localStorage.clear();
       }
+      localStorage.clear();
     }
   }
 }
